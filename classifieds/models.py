ab3938d8274e4b70729e8953a33802f2088faaa6
Jose Quezada <quezada007@yahoo.com>
2020-04-19 14:54:20 -0700

    Create the models needed for the classifieds

--- classifieds/models.py
+++ classifieds/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Classified(models.Model):
+    CATEGORIES = [
+        ('auto', 'Auto'),
+        ('free', 'Free'),
+        ('furniture', 'Furniture'),
+        ('household', 'Household'),
+        ('other', 'Other'),
+        ('room-for-rent', 'Room For Rent'),
+        ('sporting', 'Sporting'),
+        ('wanted', 'Wanted')
+    ]
+    DAYS_LISTED = [
+        ('7', '7 Days'),
+        ('14', '14 Days'),
+        ('21', '21 Days')
+    ]
+    category = models.CharField(choices=CATEGORIES, max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    submission_date = models.DateTimeField()
+    days_listed = models.CharField(choices=DAYS_LISTED, max_length=2)
+    pictures = models.ManyToManyField('Pictures', blank=True)
+
+
+class Pictures(models.Model):
+    picture = models.CharField(max_length=50)
