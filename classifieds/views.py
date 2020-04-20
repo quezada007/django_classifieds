813a27666d8eb2649ed1faa19a567190e5f3dd30
Jose Quezada <quezada007@yahoo.com>
2020-04-19 18:49:42 -0700

    Create the views for the home and classified pages

--- classifieds/views.py
+++ classifieds/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
 
-# Create your views here.
+from .models import Classified
+
+
+def home(request):
+    classifieds = Classified.objects.all()
+    return render(request, 'home.html', {'classifieds': classifieds})
+
+
+def classified_detail(request, id):
+    try:
+        classified = Classified.objects.get(id=id)
+    except Classified.DoesNotExist:
+        raise Http404('Classified Not Found')
+    return render(request, 'classified_detail.html', {'classified': classified})
