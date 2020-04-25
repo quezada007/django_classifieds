8f3a12a6c6ab553390923e0f2019a78d2ec118f5
Jose Quezada <quezada007@yahoo.com>
2020-04-24 21:15:46 -0700

    Add the submission date to the admin section

--- classifieds/admin.py
+++ classifieds/admin.py
@@ -5,4 +5,4 @@ from .models import Classified
 
 @admin.register(Classified)
 class ClassifiedAdmin(admin.ModelAdmin):
-    list_display = ['category', 'first_name', 'last_name', 'location', 'title', 'price']
+    list_display = ['category', 'first_name', 'last_name', 'location', 'title', 'price', 'submission_date']
